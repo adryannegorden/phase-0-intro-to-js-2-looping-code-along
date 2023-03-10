2557c4ee9bd7330178f90d91e328db75c5572c73
adryannegorden <adryannegorden@gmail.com>
2023-03-09 19:15:28 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+function writeCards(namesArray, eventName) {
+    const thankYou = []
+    for (let i = 0; i < namesArray.length; i++) {
+            thankYou.push(`Thank you, ${namesArray[i]}, for the wonderful surprise gift!`)
+    }
+    return(thankYou)
+}
+
+countDown(10)
